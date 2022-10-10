bee9d5ba3af8297904395e28e892562d64aeffe5
hamza@four-fox.com <hamza@four-fox.com>
2022-10-10 06:11:53 +0500

    Update customAPpBar.dart

--- lib/Views/customAPpBar.dart
+++ lib/Views/customAPpBar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mohandem_customer/Components/color.dart';
 import 'package:mohandem_customer/Components/textStyle.dart';
-import 'package:like_button/like_button.dart';
 
 import 'package:mohandem_customer/Views/Home/Repo/homeController.dart';
 
@@ -300,10 +299,8 @@ class CustomAppBarNoController extends StatelessWidget {
   final Widget? action;
   final Widget body;
   final Widget? floatingActionButton;
-  HomePageController controller = Get.find();
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
     return Container(
       color: Colors.white,
       child: SafeArea(
@@ -312,153 +309,6 @@ class CustomAppBarNoController extends StatelessWidget {
           floatingActionButton: floatingActionButton,
           floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
           backgroundColor: backgroundColor ?? DynamicColors.whiteColor,
-        //   bottomNavigationBar: fromMain == false?SizedBox.shrink():  Container(
-        //     color: Colors.white,
-        //   height: kToolbarHeight -10,
-        //   child: Padding(
-        //     padding: const EdgeInsets.only(top: 10,bottom: 10),
-        //     child: Row(
-        //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //       crossAxisAlignment: CrossAxisAlignment.center,
-        //       children: [
-        //         LikeButton(
-        //           onTap: (a) {
-        //             controller.index = 0;
-        //             controller.update();
-        //             return Future.value(true);
-        //           },
-        //
-        //           circleColor: CircleColor(
-        //               start: DynamicColors.primaryColor,
-        //               end: DynamicColors.primaryColor.withOpacity(0.5)),
-        //           bubblesColor: BubblesColor(
-        //               dotPrimaryColor: DynamicColors.primaryColor,
-        //               dotSecondaryColor:
-        //               DynamicColors.primaryColor.withOpacity(0.5)),
-        //           size: width / 5.3,
-        //           likeBuilder: (bool isLiked) {
-        //             return  Icon(
-        //               Icons.home_outlined,
-        //               size: 30,
-        //               color: controller.index == 0
-        //                   ? DynamicColors.primaryColor
-        //                   : Colors.grey,
-        //             );
-        //           },
-        //           isLiked: controller.index == 0 ? true : false,
-        //           // likeCount: likeCount,
-        //         ),
-        //         LikeButton(
-        //           onTap: (a) {
-        //             controller.index = 1;
-        //             controller.update();
-        //             return Future.value(true);
-        //           },
-        //           size: width / 5.3,
-        //           circleColor: CircleColor(
-        //               start: DynamicColors.primaryColor,
-        //               end: DynamicColors.primaryColor.withOpacity(0.5)),
-        //           bubblesColor: BubblesColor(
-        //               dotPrimaryColor: DynamicColors.primaryColor,
-        //               dotSecondaryColor:
-        //               DynamicColors.primaryColor.withOpacity(0.5)),
-        //           likeBuilder: (bool isLiked) {
-        //             return ImageIcon(
-        //               AssetImage("assets/price.png",),
-        //               size: 30,
-        //
-        //               color: controller.index == 1
-        //                   ? DynamicColors.primaryColor
-        //                   : Colors.grey,
-        //             );
-        //           },
-        //           isLiked: controller.index == 1 ? true : false,
-        //           // likeCount: likeCount,
-        //         ),
-        //         LikeButton(
-        //           onTap: (a) {
-        //             controller.index = 2;
-        //             controller.update();
-        //             return Future.value(true);
-        //           },
-        //           circleColor: CircleColor(
-        //               start: DynamicColors.primaryColor,
-        //               end: DynamicColors.primaryColor.withOpacity(0.5)),
-        //           bubblesColor: BubblesColor(
-        //               dotPrimaryColor: DynamicColors.primaryColor,
-        //               dotSecondaryColor:
-        //               DynamicColors.primaryColor.withOpacity(0.5)),
-        //           size: width / 5.3,
-        //           likeBuilder: (bool isLiked) {
-        //             return Icon(
-        //               Icons.notifications,
-        //               size: 30,
-        //               // height: kToolbarHeight - 30,
-        //               // width: 40,
-        //               color: controller.index == 2
-        //                   ? DynamicColors.primaryColor
-        //                   : Colors.grey,
-        //             );
-        //           },
-        //           isLiked: controller.index == 2 ? true : false,
-        //           // likeCount: likeCount,
-        //         ),
-        //         LikeButton(
-        //           onTap: (a) {
-        //             controller.index = 3;
-        //             controller.update();
-        //             return Future.value(true);
-        //           },
-        //           circleColor: CircleColor(
-        //               start: DynamicColors.primaryColor,
-        //               end: DynamicColors.primaryColor.withOpacity(0.5)),
-        //           bubblesColor: BubblesColor(
-        //               dotPrimaryColor: DynamicColors.primaryColor,
-        //               dotSecondaryColor:
-        //               DynamicColors.primaryColor.withOpacity(0.5)),
-        //           size: width / 5.3,
-        //           likeBuilder: (bool isLiked) {
-        //             return ImageIcon(
-        //               AssetImage("assets/myOrder.png",),
-        //               size: 30,
-        //               color: controller.index == 3
-        //                   ? DynamicColors.primaryColor
-        //                   : Colors.grey,
-        //             );
-        //           },
-        //           isLiked: controller.index == 3 ? true : false,
-        //           // likeCount: likeCount,
-        //         ),
-        //         LikeButton(
-        //           onTap: (a) {
-        //             controller.index = 4;
-        //             controller.update();
-        //             return Future.value(true);
-        //           },
-        //           circleColor: CircleColor(
-        //               start: DynamicColors.primaryColor,
-        //               end: DynamicColors.primaryColor.withOpacity(0.5)),
-        //           bubblesColor: BubblesColor(
-        //               dotPrimaryColor: DynamicColors.primaryColor,
-        //               dotSecondaryColor:
-        //               DynamicColors.primaryColor.withOpacity(0.5)),
-        //           size: width / 5.3,
-        //           likeBuilder: (bool isLiked) {
-        //             return ImageIcon(
-        //               AssetImage("assets/cart.png"),
-        //               size: 30,
-        //               color: controller.index == 4
-        //                   ? DynamicColors.primaryColor
-        //                   : Colors.grey,
-        //             );
-        //           },
-        //           isLiked: controller.index == 4 ? true : false,
-        //           // likeCount: likeCount,
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // ),
           body: Stack(
             children: [
               noAppBar == true?SizedBox.shrink(): SizedBox(
